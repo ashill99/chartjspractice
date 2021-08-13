f6903812391399a9c39eb0c2f2681f1c474f3b9f
ashill99 <admshng@gmail.com>
2021-08-13 11:02:19 -0500

    chart not working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import './script.js';
+import {Chart} from 'react-chartjs-2';
 
 function App() {
+  
+  const ctx = document.getElementById('myChart');
+
+const stars = [135850, 52122, 148825, 16939, 9763];
+const frameworks = ['React', 'Angular', 'Vue', 'Hyperapp', 'Omi'];
+
+let myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+       labels: frameworks,
+       datasets: [{
+           label: 'Popular JavaScript Frameworks',
+           data: stars
+           }]
+    },
+   });
+  
+   myChart.destroy();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+<body>
+<canvas id="myChart" width="800" height="400"></canvas>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
+  <script src="script.js"></script>
+</body>
+
   );
 }
 
